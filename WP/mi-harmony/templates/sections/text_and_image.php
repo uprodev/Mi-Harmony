9b84f811a485c344acba3559708172190ba129ef
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2024-12-19 23:42:53 +0100

    text image

---
+++ WP/mi-harmony/templates/sections/text_and_image.php
@@ -0,0 +1,46 @@
+<?php
+
+$title = get_sub_field('title');
+$items = get_sub_field('items');
+
+?>
+
+
+<section class="text-img">
+    <div class="content-width">
+        <?php if($title):?>
+            <h2><?= $title;?></h2>
+        <?php endif;?>
+        <?php if($items):
+            foreach($items as $item):
+                $pos = $item['position_text'];
+                $bg = $item['background_colour'];
+                $style = $item['image_style'];
+                $img = $item['image'];
+                $images = $item['6_images'];
+            ?>
+                <div class="item<?= $pos?' item-revers':'';?>"<?= $bg?' style="background:'.$bg.'"':'';?>>
+                    <div class="text">
+                        <?= $item['text'];?>
+                    </div>
+                    <?php if($style == '1 Image'):?>
+                        <figure>
+                            <img src="<?= $img['url'];?>" alt="<?= $img['alt'];?>">
+                        </figure>
+                    <?php elseif($style == 'No Border'):?>
+                        <figure class="no-border">
+                            <img src="<?= $img['url'];?>" alt="<?= $img['alt'];?>">
+                        </figure>
+                    <?php elseif($style == '6 Images'):?>
+                        <figure class="img-6x">
+                            <?php foreach( $images as $im ): ?>
+                                <img src="<?= $im['url'];?>" alt="<?= $im['alt'];?>">
+                            <?php endforeach;?>
+                        </figure>
+                    <?php endif;?>
+                </div>
+            <?php endforeach;
+        endif;?>
+
+    </div>
+</section>
