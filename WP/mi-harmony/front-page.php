b3cee94243c719c7ba9884f91203f209ffaf24f0
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2024-12-19 15:51:37 +0100

    home page

--- WP/mi-harmony/front-page.php
+++ WP/mi-harmony/front-page.php
@@ -4,34 +4,7 @@ get_header();
 
     get_template_part('templates/flexible');
 
-get_footer();
-
 ?>
-
-<section class="page-banner">
-    <div class="content-width">
-        <div class="text">
-            <h1>Twój przewodnik <br>
-                po świecie harmonii</h1>
-            <div class="wrap">
-                <h6>Znajdź swoją własną drogę do harmonii korzystając z jednej przestrzeni dla świata wellbeing.</h6>
-                <p>Dołącz do największej platformy z warsztatami, wyjazdami i eventami z obszaru wellbeing.</p>
-                <p>Szukaj wydarzeń, zgłębiaj techniki, poznawaj siebie.</p>
-                <p></p>
-                <p>Zostaw swój adres email - powiadomimy Cię gdy nasza platforma będzie gotowa.</p>
-                <form action="#" class="btn-wrap form-submit">
-                    <label for="email"></label>
-                    <input type="email" name="email" id="email" placeholder="TWÓj E-MAIL">
-                    <button type="submit" class="btn-bg">POWIADOM MNIE <img src="img/icon-1.svg" alt=""></button>
-                </form>
-            </div>
-
-        </div>
-        <figure>
-            <img src="img/img-1.png" alt="">
-        </figure>
-    </div>
-</section>
 
 <section class="text-img">
     <div class="content-width">
@@ -117,3 +90,5 @@ get_footer();
         </div>
     </div>
 </section>
+
+<?php get_footer();
---
+++ WP/mi-harmony/templates/sections/banner_page.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+?>
+
+<section class="page-banner">
+    <div class="content-width">
+        <div class="text">
+            <h1>Twój przewodnik <br>
+                po świecie harmonii</h1>
+            <div class="wrap">
+                <h6>Znajdź swoją własną drogę do harmonii korzystając z jednej przestrzeni dla świata wellbeing.</h6>
+                <p>Dołącz do największej platformy z warsztatami, wyjazdami i eventami z obszaru wellbeing.</p>
+                <p>Szukaj wydarzeń, zgłębiaj techniki, poznawaj siebie.</p>
+                <p></p>
+                <p>Zostaw swój adres email - powiadomimy Cię gdy nasza platforma będzie gotowa.</p>
+                <form action="#" class="btn-wrap form-submit">
+                    <label for="email"></label>
+                    <input type="email" name="email" id="email" placeholder="TWÓj E-MAIL">
+                    <button type="submit" class="btn-bg">POWIADOM MNIE <img src="img/icon-1.svg" alt=""></button>
+                </form>
+            </div>
+
+        </div>
+        <figure>
+            <img src="img/img-1.png" alt="">
+        </figure>
+    </div>
+</section>
