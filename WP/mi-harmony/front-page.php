1f9d506beb10fd9cdc8d049f99b78ee335c08308
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2024-12-19 15:13:27 +0100

    start

--- WP/mi-harmony/front-page.php
+++ WP/mi-harmony/front-page.php
@@ -5,3 +5,115 @@ get_header();
     get_template_part('templates/flexible');
 
 get_footer();
+
+?>
+
+<section class="page-banner">
+    <div class="content-width">
+        <div class="text">
+            <h1>Twój przewodnik <br>
+                po świecie harmonii</h1>
+            <div class="wrap">
+                <h6>Znajdź swoją własną drogę do harmonii korzystając z jednej przestrzeni dla świata wellbeing.</h6>
+                <p>Dołącz do największej platformy z warsztatami, wyjazdami i eventami z obszaru wellbeing.</p>
+                <p>Szukaj wydarzeń, zgłębiaj techniki, poznawaj siebie.</p>
+                <p></p>
+                <p>Zostaw swój adres email - powiadomimy Cię gdy nasza platforma będzie gotowa.</p>
+                <form action="#" class="btn-wrap form-submit">
+                    <label for="email"></label>
+                    <input type="email" name="email" id="email" placeholder="TWÓj E-MAIL">
+                    <button type="submit" class="btn-bg">POWIADOM MNIE <img src="img/icon-1.svg" alt=""></button>
+                </form>
+            </div>
+
+        </div>
+        <figure>
+            <img src="img/img-1.png" alt="">
+        </figure>
+    </div>
+</section>
+
+<section class="text-img">
+    <div class="content-width">
+        <h2>Jak to działa</h2>
+        <div class="item item-1">
+            <div class="text">
+                <h3>Wszystkie wydarzenia
+                    wellbeingowe w jednym
+                    miejscu</h3>
+                <p>Zebraliśmy dla Ciebie warsztaty jogi, mindfulness, medytacji i wiele innych wydarzeń na jednej platformie. </p>
+                <p><b>mi harmoni</b> to miejsce, gdzie znajdziesz wszelkie eventy, wyjazdy, zajęcia grupowe i terapie indywidualne z obszaru wellbeing. </p>
+            </div>
+            <figure>
+                <img src="img/img-2.jpg" alt="">
+            </figure>
+        </div>
+        <div class="item item-revers item-2">
+            <div class="text">
+                <h3>Łatwe wyszukiwanie
+                    wydarzeń</h3>
+                <p>Na platformie <b>mi harmoni</b> w szybki i prosty sposób znajdziesz to, co Cię interesuje.</p>
+                <p>Użyj wygodnych filtrów, by wyszukać warsztaty, wyjazdy i eventy. Określ technikę, datę lub rodzaj praktyk i przeglądaj gotowe wyniki.</p>
+                <p>Wyszukiwanie wydarzeń wellbeingowych jeszcze nie było tak proste!</p>
+
+
+            </div>
+            <figure>
+                <img src="img/img-3.png" alt="">
+            </figure>
+        </div>
+        <div class="item item-3">
+            <div class="text">
+                <h3>Odkrywaj <br>
+                    nowe techniki</h3>
+                <p>	W <b>mi harmoni</b> czeka na Ciebie ponad 50 technik z obszaru wellbeing: metody relaksacyjne, praca z emocjami i energią, ćwiczenia oddechowe, praktyki medytacyjne oraz wiele innych.</p>
+                <p>Odkryj różnorodne sposoby na redukcję stresu i napięć oraz walkę z uciążliwymi dolegliwościami.</p>
+                <p>Dowiedz się wszystkiego o harmonii ciała, umysłu, i emocji - poznaj nowe możliwości praktyk.</p>
+
+
+            </div>
+            <figure class="img-6x">
+                <img src="img/img-5-1.jpg" alt="">
+                <img src="img/img-5-2.jpg" alt="">
+                <img src="img/img-5-3.jpg" alt="">
+                <img src="img/img-5-4.jpg" alt="">
+                <img src="img/img-5-5.jpg" alt="">
+                <img src="img/img-5-6.jpg" alt="">
+            </figure>
+        </div>
+        <div class="item item-3">
+            <div class="text">
+                <h3>Odkrywaj <br>
+                    nowe techniki</h3>
+                <p>	W <b>mi harmoni</b> czeka na Ciebie ponad 50 technik z obszaru wellbeing: metody relaksacyjne, praca z emocjami i energią, ćwiczenia oddechowe, praktyki medytacyjne oraz wiele innych.</p>
+                <p>Odkryj różnorodne sposoby na redukcję stresu i napięć oraz walkę z uciążliwymi dolegliwościami.</p>
+                <p>Dowiedz się wszystkiego o harmonii ciała, umysłu, i emocji - poznaj nowe możliwości praktyk.</p>
+
+
+            </div>
+            <figure class="no-border">
+                <img src="img/img-6.png" alt="">
+
+            </figure>
+        </div>
+    </div>
+</section>
+
+<section class="pre-footer">
+    <div class="bg">
+        <img src="img/bg-1.jpeg" alt="">
+    </div>
+    <div class="content-width">
+        <div class="content">
+            <h2>Łączymy
+                świat wellbeingu
+                w jednym miejscu!</h2>
+            <h6>Dołącz do grona mi harmoni</h6>
+            <form action="#" class="btn-wrap form-submit">
+                <label for="email1"></label>
+                <input type="email" name="email" id="email1" placeholder="TWÓj E-MAIL">
+                <button type="submit" class="btn-bg">POWIADOM MNIE <img src="img/icon-1.svg" alt=""></button>
+            </form>
+        </div>
+    </div>
+</section>
--- WP/mi-harmony/header.php
+++ WP/mi-harmony/header.php
@@ -5,62 +5,33 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php echo wp_get_document_title(); ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-    <style>
-        .global-wrapper.page-landing{opacity:0}.preloader{position:fixed;left:0;top:0;width:100vw;height:100vh;background-color:#111;z-index:9999;display:flex;flex-direction:column;align-items:center;justify-content:center;transform-origin:center}.preloader .preloader-logo{margin:0 0 20px;opacity:0}
-    </style>
+    <?php wp_head();?>
 
-		<?php wp_head();?>
 </head>
 
 <?php
 
 $logo = get_field('logo', 'options');
-$show_preloader = get_field('show_preloader', 'options');
-$preloader_logo = get_field('preloader_logo', 'options');
-
-if(is_front_page()){
-    $style = ' page-landing bg-dark';
-}else{
-    $style_type = get_field('style' , get_the_ID());
-    if($style_type){
-        $style = ' bg-dark';
-    }else{
-        $style = ' bg-white text-dark';
-    }
-}
 
 ?>
 
 <body <?php body_class() ?>>
-<header class="header">
-    <div class="container-fluid">
-        <div class="header-logo">
-            <a href="<?= get_home_url();?>">
-                <?php if($logo):?>
-                    <img src="<?= $logo['url'];?>" alt="<?= $logo['alt'];?>" />
-                <?php endif;?>
-            </a>
+<header>
+    <div class="top-line">
+        <div class="content-width">
+            <div class="logo-wrap">
+                <a href="<?= get_home_url();?>">
+                    <?php if($logo):?>
+                        <img src="<?= $logo['url'];?>" alt="<?= $logo['alt'];?>" />
+                    <?php endif;?>
+                </a>
+            </div>
         </div>
-        <nav class="header-navigation">
-            <?php wp_nav_menu([
-                'theme_location' => 'main-menu',
-                'container' => false,
-                'menu_class' => '',
-            ]);?>
-        </nav>
     </div>
 </header>
 
-<?php if( $show_preloader):?>
-    <div id="preloader" class="preloader">
-        <div class="preloader-logo">
-            <?php if($preloader_logo):?>
-                <img src="<?= $preloader_logo['url'];?>" alt="<?= $preloader_logo['alt'];?>" />
-            <?php endif;?>
-        </div>
-    </div>
-<?php endif;?>
-
-<div class="global-wrapper<?= $style;?>">
-    <main class="content">
+<main>
